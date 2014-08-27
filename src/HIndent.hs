96ddafeee2dbe51b92b40cfed29499cd70975d8c
Chris Done <chrisdone@gmail.com>
2014-08-27 22:53:52 +0200

    Reformats

--- src/HIndent.hs
+++ src/HIndent.hs
@@ -20,16 +20,18 @@ reformat :: Text -> Either String Builder
 reformat x =
   case parseDeclWithMode parseMode
                          (T.unpack x) of
-    ParseOk v ->
-      Right (prettyPrint v)
-    ParseFailed _ e ->
-      Left e
+    ParseOk v -> Right (prettyPrint v)
+    ParseFailed _ e -> Left e
 
 -- | Pretty print the given printable thing.
 prettyPrint :: Pretty a => a -> Builder
 prettyPrint v =
   psOutput (execState (runPrinter (pretty v))
-                      (PrintState 0 mempty False 0 1))
+                      (PrintState 0
+                                  mempty
+                                  False
+                                  0
+                                  1))
 
 -- | Parse mode, includes all extensions, doesn't assume any fixities.
 parseMode :: ParseMode
@@ -43,6 +45,5 @@ parseMode =
 
 test :: Text -> IO ()
 test =
-  either error
-         (T.putStrLn . T.toLazyText) .
+  either error (T.putStrLn . T.toLazyText) .
   reformat
