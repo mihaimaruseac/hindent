2ee56bc32bd86521885cd264fbe7f793366a6537
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2014-11-04 17:20:57 -0800

    Adding line-length flag.

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -6,6 +6,7 @@
 module Main where
 
 import           HIndent
+import           HIndent.Types(Config(..))
 
 import           Data.List
 import qualified Data.Text as T
@@ -14,21 +15,27 @@ import qualified Data.Text.Lazy.IO as T
 import           Data.Version (showVersion)
 import           Paths_hindent (version)
 import           System.Environment
+import           Text.Read
 
 -- | Main entry point.
 main :: IO ()
 main =
   do args <- getArgs
      case args of
-       ["--style",findStyle -> Just style] ->
+       "--style":(findStyle -> Just style):opts ->
          T.interact
            (either error T.toLazyText .
-            reformat style)
+            reformat (modifyStyle opts style))
        ["--version"] -> putStrLn $ "hindent " ++ showVersion version
        _ ->
          error ("arguments: --style [" ++
                 intercalate "|"
                             (map (T.unpack . styleName) styles) ++
                 "]")
-  where findStyle name =
-          find ((== T.pack name) . styleName) styles
+  where 
+    findStyle name = find ((== T.pack name) . styleName) styles
+
+    modifyStyle [] style = style
+    modifyStyle ("--line-length":(readMaybe -> Just maxCol):opts) style =
+     (modifyStyle opts style) {styleDefConfig = (styleDefConfig style) {configMaxColumns = maxCol}}
+    modifyStyle args _ = error $ "Unknown args: " ++ show args
