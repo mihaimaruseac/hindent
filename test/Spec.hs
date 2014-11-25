1e40cdaa1df606fddb8670b4384ffc4b9df2a2e5
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2014-11-24 16:10:33 -0800

    Adding tests for fundamental and a test-generator executable

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,7 +13,7 @@ import qualified Data.Text.Lazy.Builder as L
 import qualified HIndent
 
 styles :: [FilePath]
-styles = ["gibiansky"]
+styles = ["fundamental", "gibiansky"]
 
 testDir, expectedDir :: String
 testDir = "tests"
