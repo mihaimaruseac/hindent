c8f95b2689e1df756c48081f4448c8277f085178
Tony Day <tonyday567@gmail.com>
2015-01-26 14:30:45 +1000

    snapshot for Pipes

--- src/HIndent/AST.hs
+++ src/HIndent/AST.hs
@@ -6,20 +6,18 @@
 module HIndent.AST where
 
 import           HIndent.Types
-
 import           Control.Monad.State.Strict
 import           Data.Data
-import           Data.Function
 import           Data.Traversable
 import           Language.Haskell.Exts.Annotated
 import Data.Monoid
 
+-- data Direction = Before | After
 data Attachment = HighestLayer | LowestLayer deriving (Eq)
-
 data Location = Before' | After' | Inside | Wrapping | Overlapping | Same deriving (Eq)
 
 
-okDirection :: Location -> Direction -> Bool
+okDirection :: Location -> ComInfoLocation -> Bool
 okDirection l d = (l == Before' && d == Before) ||
                   (l == After' && d == After)
 
@@ -31,34 +29,32 @@ commentLocation (Comment _ c _) (NodeInfo (SrcSpanInfo n _) _) =
 -- | the location of the first span in terms of the second
 location :: SrcSpan -> SrcSpan -> Location
 location first second
-  | end first < start second = Before'
-  | start first > end second = After'
-  | start first == start second && end first == end second = Same
-  | start first >= start second && end first <= end second = Inside
-  | start first <= start second && end first >= end second = Wrapping
+  | srcSpanEnd first < srcSpanStart second = Before'
+  | srcSpanStart first > srcSpanEnd second = After'
+  | srcSpanStart first == srcSpanStart second && 
+    srcSpanEnd first == srcSpanEnd second = Same
+  | srcSpanStart first >= srcSpanStart second && 
+    srcSpanEnd first <= srcSpanEnd second = Inside
+  | srcSpanStart first <= srcSpanStart second && 
+    srcSpanEnd first >= srcSpanEnd second = Wrapping
   | otherwise = Overlapping
-
-start :: SrcSpan -> (Int,Int)
-start s = (srcSpanStartLine s, srcSpanStartColumn s)
-
-end :: SrcSpan -> (Int,Int)
-end s = (srcSpanEndLine s, srcSpanEndColumn s)
-
-instance (Num a,Num b) => Num (a, b) where
-   (a,b) - (c,d) = (a-c,b-d)
 
 -- | starting with a reference span, is the first span better than the second
 -- for attaching a comment (closer, the right direction and the right shape)?
-better :: Attachment -> Direction -> SrcSpan -> SrcSpan -> SrcSpan -> Bool
+better :: Attachment -> ComInfoLocation -> SrcSpan -> SrcSpan -> SrcSpan -> Bool
 better att dir ref first second
-  | not (okDirection (location ref first) dir) = False
   | not (okDirection (location ref second) dir) = True
-  | start first - end ref < start second - end ref = True
-  | start first - end ref > start second - end ref = False
+  | not (okDirection (location ref second) dir) = False
+  | srcSpanStart first `diff` srcSpanEnd ref < srcSpanStart second `diff` srcSpanEnd ref =
+      dir == Before
+  | srcSpanStart first `diff` srcSpanEnd ref > srcSpanStart second `diff` srcSpanEnd ref = dir == After
   | otherwise = case location first second of
                  Inside -> att == LowestLayer
                  Wrapping -> att == HighestLayer
                  _ -> True
+  where
+    diff :: (Int,Int) -> (Int,Int) -> (Int,Int)
+    diff (a,b) (c,d) = (a-c,b-d)
 
 addId :: (Traversable ast) => ast a -> ast (a, Int)
 addId ast =
@@ -72,9 +68,9 @@ addId ast =
 removeId :: (Traversable ast) => ast (a,b) -> ast a
 removeId ast = fmap fst ast
 
-findBest :: (Traversable ast) => Attachment -> Direction -> Comment -> ast (NodeInfo,Int) -> Maybe (NodeInfo,Int)
+findBest :: (Traversable ast) => Attachment -> ComInfoLocation -> Comment -> ast (NodeInfo,Int) -> Maybe (NodeInfo,Int)
 findBest att dir (Comment _ c _) ast =
-  execState (traverse (\x' -> modify (\x -> maybeBetter x x')) ast) Nothing
+  execState (traverse (\x' -> modify (`maybeBetter` x')) ast) Nothing
   where
     maybeBetter Nothing x = Just x
     maybeBetter
@@ -92,7 +88,7 @@ insertById comment n ast =
     else a)
   ast
 
-insertComment :: Traversable ast => Attachment-> Direction-> Comment-> ([ComInfo],ast NodeInfo) -> ([ComInfo],ast NodeInfo)
+insertComment :: Traversable ast => Attachment-> ComInfoLocation-> Comment-> ([ComInfo],ast NodeInfo) -> ([ComInfo],ast NodeInfo)
 insertComment att dir comment (cs,ast) =
   let ast' = addId ast
       best = findBest att dir comment ast'
@@ -108,153 +104,42 @@ placeComments att ast comments =
       -- Make sure to process comments top to bottom.
       reversed = reverse comments
       -- Add all comments to the ast.
-      (cominfos, ast'') = foldr processComment ([], ast') reversed
+      (topcomments, ast'') = foldr (placeComment att) ([], ast') reversed
 
   in -- Reverse order of comments at each node.
-    (cominfos, fmap (\(NodeInfo n cs) -> NodeInfo n $ reverse cs) ast'')
-
-  where processComment :: Comment
-                       -> ([ComInfo],ast NodeInfo)
-                       -> ([ComInfo],ast NodeInfo)
-        -- Add in a single comment to the ast.
-        processComment c@(Comment _ cspan _) (cs,ast) =
-          -- Try to find the best previous node
-          case findBest att After c (addId ast) of
-            Nothing -> insertComment att Before c (cs,ast)
-            -- We found the node that this comment follows.
-            -- Check whether the node is on the same line.
-            Just ((NodeInfo n coms),_)
-              -- If it's on a different line than the node, but the node has an
-              -- EOL comment, and the EOL comment and this comment are aligned,
-              -- attach this comment to the preceding node.
-              | ownLine && alignedWithPrevious ->
-                  insertComment att After c (cs,ast)
-              -- otherwise attach to the next node
-              | otherwise -> insertComment att Before c (cs,ast)
-
-              where
-                ownLine = srcSpanStartLine cspan /= srcSpanEndLine (srcInfoSpan n)
-                alignedWithPrevious
-                  | null coms = False
-                  | otherwise = case last coms of
-                      -- Require single line comment after the node.
-                      ComInfo (Comment False prevSpan _) (Just After) ->
-                        srcSpanStartLine prevSpan == srcSpanStartLine cspan - 1 &&
-                        srcSpanStartColumn prevSpan == srcSpanStartColumn cspan
-                      _       -> False
+    (topcomments, fmap (\(NodeInfo n cs) -> NodeInfo n $ reverse cs) ast'')
 
 
--- | Annotate the AST with comments.
-annotateComments :: forall ast. (Data (ast NodeInfo),Traversable ast,Annotated ast)
-                 => ast SrcSpanInfo -> [Comment] -> ([ComInfo],ast NodeInfo)
-annotateComments src comments =
-  let
-      -- Make sure to process comments top to bottom.
-      reversed = reverse comments
+placeComment 
+  :: (Traversable ast)
+  => Attachment
+  -> Comment
+  -> ([ComInfo],ast NodeInfo)
+  -> ([ComInfo],ast NodeInfo)
+placeComment att c@(Comment _ cspan _) (cs,ast) =
+  -- Try to find the best node before this comment
+  case findBest att After c (addId ast) of
+    -- no Node exists before this comment. Place it with the node after
+    Nothing -> insertComment att Before c (cs,ast)
+    -- A node before the comment exists
+    Just (NodeInfo n coms,_)
+      -- If it's on the same line, or 
+      -- it's on a different line than the node, but the node has an
+      -- EOL comment, and the EOL comment and this comment are aligned,
+      -- attach this comment to the preceding node.
+      | sameLine || (not sameLine && alignedWithPrevious) ->
+          insertComment att After c (cs,ast)
+      -- otherwise attach to the next node
+      | otherwise -> insertComment att Before c (cs,ast)
+      where
+        sameLine = srcSpanStartLine cspan == srcSpanEndLine (srcInfoSpan n)
+        alignedWithPrevious
+          | null coms = False
+          | otherwise = case last coms of
+              -- Require single line comment after the node.
+              ComInfo (Comment False prevSpan _) (Just After) ->
+                srcSpanStartLine prevSpan == srcSpanStartLine cspan - 1 &&
+                srcSpanStartColumn prevSpan == srcSpanStartColumn cspan
+              _       -> False
 
-      -- Replace source spans with node infos in the AST.
-      src' = fmap (\n -> NodeInfo n []) src
-
-      -- Add all comments to the ast.
-      (cominfos, src'') = foldr processComment ([], src') reversed
-
-  in -- Reverse order of comments at each node.
-    (cominfos, fmap (\(NodeInfo n cs) -> NodeInfo n $ reverse cs) src'')
-
-  where processComment :: Comment
-                       -> ([ComInfo],ast NodeInfo)
-                       -> ([ComInfo],ast NodeInfo)
-        -- Add in a single comment to the ast.
-        processComment c@(Comment _ cspan _) (cs,ast) =
-          -- Try to find the node after which this comment lies.
-          case execState (traverse (collect After c) ast) Nothing of
-            -- When no node is found, the comment is a leading comment of the ast
-            -- try and find the next node
-            Nothing -> case execState (traverse (collect Before c) ast) Nothing of
-
-              Nothing -> (ComInfo c Nothing : cs, ast)
-              Just (NodeInfo l _) ->
-                (cs, evalState (traverse (insert l (ComInfo c $ Just Before)) ast) False)
-
-
-            -- We found the node that this comment follows.
-            -- Check whether the node is on the same line.
-            Just (NodeInfo l coms)
-              -- If it's on a different line than the node, but the node has an
-              -- EOL comment, and the EOL comment and this comment are aligned,
-              -- attach this comment to the preceding node.
-              | ownLine && alignedWithPrevious -> insertedBefore
-
-              -- If it's on a different line than the node, look for the following node to attach it to.
-              | ownLine ->
-                  case execState (traverse (collect Before c) ast) Nothing of
-                    -- If we don't find a node after the comment, leave it with the previous node.
-                    Nothing   -> insertedBefore
-                    Just (NodeInfo node _) ->
-                      (cs, evalState (traverse (insert node (ComInfo c $ Just Before)) ast) False)
-
-              -- If it's on the same line, insert this comment into that node.
-              | otherwise -> insertedBefore
-              where
-                ownLine = srcSpanStartLine cspan /= srcSpanEndLine (srcInfoSpan l)
-                insertedBefore = (cs, evalState (traverse (insert l (ComInfo c $ Just After)) ast) False)
-                alignedWithPrevious
-                  | null coms = False
-                  | otherwise = case last coms of
-                      -- Require single line comment after the node.
-                      ComInfo (Comment False prevSpan _) (Just After) ->
-                        srcSpanStartLine prevSpan == srcSpanStartLine cspan - 1 &&
-                        srcSpanStartColumn prevSpan == srcSpanStartColumn cspan
-                      _       -> False
-
-        -- For a comment, check whether the comment is after the node.
-        -- If it is, store it in the state; otherwise do nothing.
-        -- The location specifies where the comment should lie relative to the node.
-        collect :: Direction -> Comment -> NodeInfo -> State (Maybe NodeInfo) NodeInfo
-        collect loc' c ni@(NodeInfo newL _) =
-          do when (commentLocated loc' ni c)
-                  (modify (maybe (Just ni)
-                                 (\oldni@(NodeInfo oldL _) ->
-                                    Just (if (spanTest loc' `on` srcInfoSpan) oldL newL
-                                             then ni
-                                             else oldni))))
-             return ni
-
-        -- Insert the comment into the ast. Find the right node and add it to the
-        -- comments of that node. Do nothing afterwards.
-        insert :: SrcSpanInfo -> ComInfo -> NodeInfo -> State Bool NodeInfo
-        insert al c ni@(NodeInfo bl cs) =
-          do done <- get
-             if not done && al == bl
-                then do put True
-                        return (ni {nodeInfoComments = c : cs})
-                else return ni
-
--- | Is the comment before/after the node?
-commentLocated :: Direction -> NodeInfo -> Comment -> Bool
-commentLocated loc' (NodeInfo (SrcSpanInfo n _) _) (Comment _ c _) =
-  spanTest loc' n c
-
--- | For @After@, does the first span end before the second starts?
--- For @Before@, does the first span start after the second ends?
-spanTest :: Direction -> SrcSpan -> SrcSpan -> Bool
-spanTest loc' first second =
-  
-  (srcSpanStartLine after > srcSpanEndLine before) ||
-  ((srcSpanStartLine after == srcSpanEndLine before) &&
-   (srcSpanStartColumn after > srcSpanEndColumn before))
-  where (before,after) =
-          case loc' of
-            After -> (first,second)
-            Before -> (second,first)
-
-spanTestNode :: Direction -> SrcSpan -> SrcSpan -> Bool
-spanTestNode loc' first second =
-  (srcSpanStartLine after > srcSpanStartLine before) ||
-  ((srcSpanStartLine after == srcSpanStartLine before) &&
-   (srcSpanEndColumn after > srcSpanEndColumn before))
-  where (before,after) =
-          case loc' of
-            After -> (first,second)
-            Before -> (second,first)
 
--- test/tony-day/testParsing.hs
+++ test/tony-day/testParsing.hs
@@ -9,6 +9,7 @@ import           Data.Either
 import qualified Data.Text.Lazy as Text
 import qualified Data.Text.Lazy.Builder as Text
 import qualified Data.Text.Lazy.IO as Text
+import Language.Haskell.HLint3
 import           HIndent
 import HIndent.AST
 import           HIndent.Merge
@@ -18,6 +19,9 @@ import           HIndent.Types
 import           Language.Haskell.Exts.Annotated hiding (Style,prettyPrint,Pretty,style,parse)
 import           Control.Monad.State.Strict
 import Data.Typeable
+import Text.Show.Pretty hiding (Con)
+import Text.PrettyPrint.HughesPJ (Doc)
+import Data.Functor.Identity
 
 t1 :: String
 t1 = "test/tony-day/test1.hs"
@@ -28,62 +32,73 @@ tc1 = Text.pack "{- multi -}\nx=1\n{- multi -}\nx'=2\n\n{- multi -}"
 tc2 :: Text.Text
 tc2 = Text.pack "-- single\nx=1\n-- single\nx'=2\n\n-- single"
 
-testf :: Style -> FilePath -> IO ()
-testf style f = do
-  tf1 <- Text.readFile f
-  let ast1 = testAst tf1
-  case ast1 of
-   Left _ -> return ()
-   Right (cominfo, m) ->
-     let p = prettyPrint style
-             (do
-                 mapM_ (printComment Nothing) (reverse cominfo)
-                 pretty m)
-     in
-     Text.putStr $ Text.toLazyText p
+file :: FilePath -> IO Text.Text
+file = Text.readFile
 
-getAst :: FilePath -> IO [Module ()]
-getAst f = do
-  tf1 <- Text.readFile f
-  return $ (bare . snd) <$> rights [testAst tf1]
+showText :: Text.Text -> IO ()
+showText t = 
+  Text.putStr t
 
-getAstC :: FilePath -> IO ([ComInfo], Module NodeInfo)
-getAstC f = do
-  t <- Text.readFile f
-  let (m, c) = fromParseResult (parseModuleWithComments parseMode (Text.unpack t))
-      (x, ast) = annotateComments m c
-  return (x, ast)
-
-getAstC' :: Text.Text -> IO ([ComInfo], Module NodeInfo)
-getAstC' t = do
-  let (m, c) = fromParseResult (parseModuleWithComments parseMode (Text.unpack t))
-      (x, ast) = annotateComments m c
-  return (x, ast)
+show' :: (Show a) => a -> Doc
+show' = ppDoc 
 
 getParse :: Text.Text -> (Module SrcSpanInfo, [Comment])
 getParse t = fromParseResult (parseModuleWithComments parseMode (Text.unpack t))
 
-getAnn :: Text.Text -> ([ComInfo], Module NodeInfo)
-getAnn = uncurry (placeComments HighestLayer) . getParse
+getAst :: Text.Text -> ([ComInfo], Module NodeInfo)
+getAst = uncurry (placeComments HighestLayer) . getParse
 
-testt :: Text.Text -> IO ()
-testt t =
-  let (cominfo,m) = getAnn t
-      p = prettyPrint tonyDay $ do
-        mapM_ (printComment Nothing) (reverse cominfo)
-        pretty m
-  in
-  Text.putStr $ Text.toLazyText p
+prettyAst :: Pretty ast => Style -> ([ComInfo], ast NodeInfo) -> Text.Builder
+prettyAst style (topcomments, ast) = prettyPrint style $ do
+  mapM_ (printComment Nothing) (reverse topcomments)
+  pretty ast
 
-testt' :: Text.Text -> IO ()
-testt' t =
-  let (cominfo,m) = getAnn t
-      p = prettyPrint fundamental $ do
-        mapM_ (printComment Nothing) (reverse cominfo)
-        pretty m
-  in
-  Text.putStr $ Text.toLazyText p
+getPretty :: Style -> Text.Text -> Text.Text
+getPretty style = Text.toLazyText . prettyAst style . getAst  
 
+getDecl :: Module NodeInfo -> Decl NodeInfo
+getDecl (Module _ _ _ _ decls) = head decls
+
+showT :: (Traversable t, Show a) => t a -> [String]
+showT ast = execState (traverse (\x -> modify (\xs -> show x:xs)) ast) []
+
+-- idT :: Num s => StateT s Identity a -> s
+idT :: Module SrcSpanInfo -> Module Int
+idT ast = execState (traverse (\ann' ->
+                                (inc ann')) ast) 0
+  where
+    inc :: t -> State Int Int
+    inc _ = do
+      modify (+1)
+      get
+      
+-- posnT :: (Traversable t, Typeable a) => t a -> Int
+posnT :: Module SrcSpanInfo -> Module ((Int, Int), (Int, Int))
+posnT ast = fmap (\(SrcSpanInfo (SrcSpan _ l0 c0 l1 c1) _)
+                      -> ((l0,c0),(l1,c1)) ) ast
+
+posnT' :: Functor f => f NodeInfo -> f ((Int, Int), (Int, Int), [ComInfo])
+posnT' ast = fmap (\(NodeInfo (SrcSpanInfo (SrcSpan _ l0 c0 l1 c1) _) cs)
+                      -> ((l0,c0),(l1,c1),cs) ) ast
+
+
+
+-- show' $ posnT $ fst $ getParse "x1 = putStrLn . show"
+-- show' $ posnT' $ snd $ getAst "x1 = putStrLn . show {- end -}"
+
+-- hlint
+getDeclsIdea :: Text.Text -> IO [Idea]
+getDeclsIdea t = do
+  (flags, classify, hint) <- autoSettings
+  let (m@(Module _ _ _ _ decls),c) = getParse t
+      scope = scopeCreate m
+  return $ concat $ hintDecl hint scope m <$> decls
+
+
+
+
+
+{-
 testMerge :: FilePath -> FilePath -> IO ()
 testMerge f f' = do
   t <- Text.readFile f
@@ -105,17 +120,6 @@ testMerge f f' = do
      in
      Text.putStr $ Text.toLazyText p
 
-getDecl :: Module NodeInfo -> Decl NodeInfo
-getDecl (Module _ _ _ _ decls) = head decls
-
-
-
-t2 :: (Traversable t, Show a) => t a -> [String]
-t2 ast = execState (traverse (\x -> modify (\xs -> (show x):xs)) ast) []
-
-tCount :: (Traversable t, Typeable a) => t a -> Int
-tCount ast = execState (traverse (\x -> modify (\xs -> xs+1)) ast) 0
-
 
 order :: (Traversable t, Num s) => t a -> (t s, s)
 order ast =
@@ -128,5 +132,28 @@ order ast =
    )
    ast)
   0
+-}
+
+-- show' $ posnT $ fst $ getParse "Proxy a a' b b' m"
 
 
+isProxy :: Exp t -> Bool
+isProxy (Con _ (UnQual _ (Ident _ i))) = i == "Proxy"
+isProxy _ = False
+
+isX (UnQual _ (Ident _ i)) = i == "X"
+isX _ = False
+
+isUnit (Special _ (UnitCon _)) = True
+isUnit _ = False
+
+ident (Var _ (UnQual _ (Ident _ i))) = Just i
+ident _ = Nothing
+
+data S = S { foundProxy :: Bool }
+
+{-
+lookForProxy ast
+  | isProxy = modify (\x -> x {foundProxy=True})
+  | isUnit =
+-}
