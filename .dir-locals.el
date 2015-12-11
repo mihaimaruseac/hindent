f53f69837bf6f3f30788c351edd5dee707271c37
Chris Done <chrisdone@gmail.com>
2015-12-11 10:14:02 +0100

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,10 @@
-((nil . ((hindent-style . "chris-done"))))
+((haskell-mode
+  . ((haskell-indent-spaces . 2)
+     (hindent-style . "chris-done")
+     (haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng"))))
+ (haskell-cabal-mode
+  . ((haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng")))))
