64a6c228dbd256e837122275b5d9b938a2658ec9
Enno Cramer <ecramer@memfrob.de>
2016-02-12 16:16:51 +0100

    Add tests for style 'Cramer'

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,7 +15,7 @@ import Language.Haskell.Exts.Extension
 import qualified HIndent
 
 styles :: [FilePath]
-styles = ["fundamental", "gibiansky", "chris-done", "johan-tibell"]
+styles = ["fundamental", "gibiansky", "chris-done", "johan-tibell", "cramer"]
 
 testDir, expectedDir :: String
 testDir = "tests"
