11a7b4a9a91ed4f7ca42d7fdc148aaa1ae30c3c4
Hiroki Tokunaga <tokusan441@gmail.com>
2022-12-27 09:30:21 +0900

    Format

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,20 +1,20 @@
-{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Benchmark the pretty printer.
 module Main where
 
-import           Control.DeepSeq
-import           Criterion
-import           Criterion.Main
-import qualified Data.ByteString                as S
-import qualified Data.ByteString.Builder        as S
-import qualified Data.ByteString.Char8          as S8
-import qualified Data.ByteString.UTF8           as UTF8
-import           HIndent
-import           HIndent.Config
-import           HIndent.Internal.Test.Markdone
-import           HIndent.LanguageExtension
+import Control.DeepSeq
+import Criterion
+import Criterion.Main
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Builder as S
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.ByteString.UTF8 as UTF8
+import HIndent
+import HIndent.Config
+import HIndent.Internal.Test.Markdone
+import HIndent.LanguageExtension
 
 -- | Main benchmarks.
 main :: IO ()
