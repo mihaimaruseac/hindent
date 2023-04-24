657237b4c58b700681af6ef0ed36a054950408f8
Hiroki Tokunaga <tokusan441@gmail.com>
2023-04-24 21:19:54 +0900

    Create 'HIndent.CommandlineOptions'

---
+++ src/HIndent/CommandlineOptions.hs
@@ -0,0 +1,87 @@
+module HIndent.CommandlineOptions
+  ( Action(..)
+  , RunMode(..)
+  , options
+  ) where
+
+import Data.Maybe
+import qualified Data.Text as T
+import HIndent.Config
+import HIndent.LanguageExtension
+import HIndent.LanguageExtension.Types
+import Options.Applicative hiding (action, style)
+
+data Action
+  = Validate
+  | Reformat
+
+data RunMode
+  = ShowVersion
+  | Run Config [Extension] Action [FilePath]
+
+-- | Program options.
+options :: Config -> Parser RunMode
+options config =
+  flag' ShowVersion (long "version" <> help "Print the version") <|>
+  (Run <$> style <*> exts <*> action <*> files)
+  where
+    style =
+      (makeStyle config <$> lineLen <*> indentSpaces <*> trailingNewline <*>
+       sortImports) <*
+      optional
+        (strOption
+           (long "style" <>
+            help "Style to print with (historical, now ignored)" <>
+            metavar "STYLE") :: Parser String)
+    exts =
+      fmap
+        getExtensions
+        (many
+           (T.pack <$>
+            strOption
+              (short 'X' <> help "Language extension" <> metavar "GHCEXT")))
+    indentSpaces =
+      option
+        auto
+        (long "indent-size" <>
+         help "Indentation size in spaces" <>
+         value (configIndentSpaces config) <> showDefault) <|>
+      option
+        auto
+        (long "tab-size" <> help "Same as --indent-size, for compatibility")
+    lineLen =
+      option
+        auto
+        (long "line-length" <>
+         help "Desired length of lines" <>
+         value (configMaxColumns config) <> showDefault)
+    trailingNewline =
+      not <$>
+      flag
+        (not (configTrailingNewline config))
+        (configTrailingNewline config)
+        (long "no-force-newline" <>
+         help "Don't force a trailing newline" <> showDefault)
+    sortImports =
+      flag
+        Nothing
+        (Just True)
+        (long "sort-imports" <> help "Sort imports in groups" <> showDefault) <|>
+      flag
+        Nothing
+        (Just False)
+        (long "no-sort-imports" <> help "Don't sort imports")
+    action =
+      flag
+        Reformat
+        Validate
+        (long "validate" <>
+         help "Check if files are formatted without changing them")
+    makeStyle s mlen tabs trailing imports =
+      s
+        { configMaxColumns = mlen
+        , configIndentSpaces = tabs
+        , configTrailingNewline = trailing
+        , configSortImports = fromMaybe (configSortImports s) imports
+        }
+    files = many (strArgument (metavar "FILENAMES"))
