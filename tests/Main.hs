e25a83374c6328ebc5a4e4b431499c019a280946
Hiroki Tokunaga <tokusan441@gmail.com>
2023-05-03 10:52:01 +0900

    Export only the `HIndent` module
    To hide the implementation.

--- tests/Main.hs
+++ tests/Main.hs
@@ -7,7 +7,6 @@ module Main where
 import Data.Algorithm.Diff
 import Data.Algorithm.DiffOutput
 import qualified Data.ByteString as S
-import qualified Data.ByteString.Builder as S
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
@@ -16,9 +15,7 @@ import qualified Data.ByteString.UTF8 as UTF8
 import Data.Function
 import Data.Version
 import qualified HIndent
-import HIndent.CodeBlock
-import HIndent.Config
-import HIndent.Error
+import HIndent (Config(..))
 import HIndent.Internal.Test.Markdone
 import qualified System.Info
 import Test.Hspec
@@ -31,20 +28,19 @@ main = do
   bytes <- S.readFile "TESTS.md"
   forest <- parse (tokenize bytes)
   hspec $ do
-    codeBlocksSpec
     markdoneSpec
     toSpec forest
 
 reformat :: Config -> S.ByteString -> ByteString
 reformat cfg code =
-  either (("-- " <>) . L8.pack . prettyParseError) L.fromStrict $
+  either (("-- " <>) . L8.pack . HIndent.prettyParseError) L.fromStrict $
   HIndent.reformat cfg [] Nothing code
 
 -- | Convert the Markdone document to Spec benchmarks.
 toSpec :: [Markdone] -> Spec
 toSpec = go
   where
-    cfg = HIndent.Config.defaultConfig {configTrailingNewline = False}
+    cfg = HIndent.defaultConfig {configTrailingNewline = False}
     go (Section name children:next) = do
       describe (UTF8.toString name) (go children)
       go next
@@ -131,39 +127,6 @@ diff x y = ppDiff (on getGroupedDiff (lines . LUTF8.toString) x y)
 skipEmptyLines :: [Markdone] -> [Markdone]
 skipEmptyLines (PlainText "":rest) = rest
 skipEmptyLines other = other
-
-codeBlocksSpec :: Spec
-codeBlocksSpec =
-  describe "splitting source into code blocks" $ do
-    it "should put just Haskell code in its own block" $ do
-      let input = "this is totally haskell code\n\nit deserves its own block!\n"
-      cppSplitBlocks input `shouldBe` [HaskellSource 0 input]
-    it "should put #if/#endif and Haskell code into separate blocks" $ do
-      cppSplitBlocks
-        "haskell code\n#if DEBUG\ndebug code\n#endif\nmore haskell code\n" `shouldBe`
-        [ HaskellSource 0 "haskell code"
-        , CPPDirectives "#if DEBUG"
-        , HaskellSource 2 "debug code"
-        , CPPDirectives "#endif"
-        , HaskellSource 4 "more haskell code\n"
-        ]
-    it "should put the shebang line into its own block" $ do
-      cppSplitBlocks
-        "#!/usr/bin/env runhaskell\n{-# LANGUAGE OverloadedStrings #-}\n" `shouldBe`
-        [ Shebang "#!/usr/bin/env runhaskell"
-        , HaskellSource 1 "{-# LANGUAGE OverloadedStrings #-}\n"
-        ]
-    it "should put a multi-line #define into its own block" $ do
-      let input = "#define A \\\n  macro contents \\\n  go here\nhaskell code\n"
-      cppSplitBlocks input `shouldBe`
-        [ CPPDirectives "#define A \\\n  macro contents \\\n  go here"
-        , HaskellSource 3 "haskell code\n"
-        ]
-    it "should put an unterminated multi-line #define into its own block" $ do
-      cppSplitBlocks "#define A \\" `shouldBe` [CPPDirectives "#define A \\"]
-      cppSplitBlocks "#define A \\\n" `shouldBe` [CPPDirectives "#define A \\"]
-      cppSplitBlocks "#define A \\\n.\\" `shouldBe`
-        [CPPDirectives "#define A \\\n.\\"]
 
 markdoneSpec :: Spec
 markdoneSpec = do
