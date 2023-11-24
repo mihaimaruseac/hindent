ad0e23dacd5b1cf6e3535a98ae325b758ef23f28
Mihai Maruseac <mihai.maruseac@gmail.com>
2023-11-24 15:41:17 -0500

    Update shell.nix file based on tutorial (#792)
    * Update shell.nix file based on tutorial
    
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>
    
    * Update shell.nix file based on tutorial
    
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>
    
    ---------
    
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,11 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-mkShell {
-  buildInputs = [
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
     cabal-install
     haskell.compiler.ghc925
     zlib.dev
   ];
+
   shellHook = ''
     export PS1="[\[\033[01;32m\]nix-shell\[\033[00m\]:\W] \[\033[01;32m\]λ\[\033[00m\] "
   '';
