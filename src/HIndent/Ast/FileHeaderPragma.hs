ca4516874c5da49b180fe4398da2ad0f6a902fc0
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-27 10:59:57 +0900

    Imprement `Pretty FileHeaderPragmaCollection`

--- src/HIndent/Ast/FileHeaderPragma.hs
+++ src/HIndent/Ast/FileHeaderPragma.hs
@@ -1,5 +1,21 @@
 module HIndent.Ast.FileHeaderPragma
   ( FileHeaderPragma
+  , mkFileHeaderPragma
   ) where
 
-data FileHeaderPragma
+import HIndent.Ast.NodeComments
+import HIndent.Pretty
+import HIndent.Pretty.Combinators
+import HIndent.Pretty.NodeComments
+
+newtype FileHeaderPragma =
+  FileHeaderPragma String
+
+instance CommentExtraction FileHeaderPragma where
+  nodeComments _ = NodeComments [] [] []
+
+instance Pretty FileHeaderPragma where
+  pretty' (FileHeaderPragma x) = string x
+
+mkFileHeaderPragma :: String -> FileHeaderPragma
+mkFileHeaderPragma = FileHeaderPragma
--- src/HIndent/Ast/FileHeaderPragma/Collection.hs
+++ src/HIndent/Ast/FileHeaderPragma/Collection.hs
@@ -1,12 +1,65 @@
+{-# LANGUAGE CPP #-}
+
 module HIndent.Ast.FileHeaderPragma.Collection
   ( FileHeaderPragmaCollection
   , mkFileHeaderPragmaCollection
   ) where
 
+import Data.Bifunctor
+import Data.Char
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import Generics.SYB
 import HIndent.Ast.FileHeaderPragma
+import HIndent.Ast.NodeComments
+import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
+import HIndent.Pragma
+import HIndent.Pretty
+import HIndent.Pretty.Combinators
+import HIndent.Pretty.NodeComments
 
 newtype FileHeaderPragmaCollection =
   FileHeaderPragmaCollection [FileHeaderPragma]
 
-mkFileHeaderPragmaCollection :: a -> FileHeaderPragmaCollection
-mkFileHeaderPragmaCollection _ = FileHeaderPragmaCollection []
+instance CommentExtraction FileHeaderPragmaCollection where
+  nodeComments _ = NodeComments [] [] []
+
+instance Pretty FileHeaderPragmaCollection where
+  pretty' (FileHeaderPragmaCollection xs) = lined $ fmap pretty xs
+
+mkFileHeaderPragmaCollection :: GHC.HsModule' -> FileHeaderPragmaCollection
+mkFileHeaderPragmaCollection =
+  FileHeaderPragmaCollection . fmap mkFileHeaderPragma . collectPragmas
+
+-- | This function collects pragma comments from the
+-- given module and modifies them into 'String's.
+--
+-- A pragma's name is converted to the @SHOUT_CASE@ (e.g., @lAnGuAgE@ ->
+-- @LANGUAGE@).
+collectPragmas :: GHC.HsModule' -> [String]
+collectPragmas =
+  fmap (uncurry constructPragma)
+    . mapMaybe extractPragma
+    . listify isBlockComment
+    . GHC.getModuleAnn
+
+-- | This function returns a 'Just' value with the pragma
+-- extracted from the passed 'EpaCommentTok' if it has one. Otherwise, it
+-- returns a 'Nothing'.
+extractPragma :: GHC.EpaCommentTok -> Maybe (String, [String])
+extractPragma (GHC.EpaBlockComment c) =
+  second (fmap strip . splitOn ",") <$> extractPragmaNameAndElement c
+  where
+    strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+extractPragma _ = Nothing
+
+-- | Construct a pragma.
+constructPragma :: String -> [String] -> String
+constructPragma optionOrPragma xs =
+  "{-# " ++ fmap toUpper optionOrPragma ++ " " ++ intercalate ", " xs ++ " #-}"
+
+-- | Checks if the given comment is a block one.
+isBlockComment :: GHC.EpaCommentTok -> Bool
+isBlockComment GHC.EpaBlockComment {} = True
+isBlockComment _ = False
