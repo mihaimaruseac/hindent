685c27450ca91563e55259842a95bc0994d51468
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-27 12:01:58 +0900

    Remove an unnecessary CPP block

--- src/HIndent/Ast/Module.hs
+++ src/HIndent/Ast/Module.hs
@@ -31,7 +31,7 @@ data Module = Module
 
 instance CommentExtraction Module where
   nodeComments Module {} = NodeComments [] [] []
-#if MIN_VERSION_ghc_lib_parser(9,6,1)
+
 instance Pretty Module where
   pretty' Module {module' = m@GHC.HsModule { hsmodName = Nothing
                                            , hsmodImports = []
@@ -68,44 +68,7 @@ instance Pretty Module where
         gets (configSortImports . psConfig) >>= \case
           True -> pure $ extractImportsSorted m
           False -> pure $ extractImports m
-#else
-instance Pretty Module where
-  pretty' Module {module' = m@GHC.HsModule { hsmodName = Nothing
-                                           , hsmodImports = []
-                                           , hsmodDecls = []
-                                           }}
-    | not (pragmaExists m) = pure ()
-  pretty' Module {module' = m, ..} = blanklined printers >> newline
-    where
-      printers = snd <$> filter fst pairs
-      pairs =
-        [ (hasPragmas pragmas, pretty pragmas)
-        , (isJust moduleDeclaration, prettyModuleDecl moduleDeclaration)
-        , (importsExist m, prettyImports)
-        , (declsExist m, prettyDecls)
-        ]
-      prettyModuleDecl Nothing = error "The module declaration does not exist."
-      prettyModuleDecl (Just decl) = pretty decl
-      prettyDecls =
-        mapM_ (\(x, sp) -> pretty x >> fromMaybe (return ()) sp)
-          $ addDeclSeparator
-          $ GHC.hsmodDecls m
-      addDeclSeparator [] = []
-      addDeclSeparator [x] = [(x, Nothing)]
-      addDeclSeparator (x:xs) =
-        (x, Just $ declSeparator $ GHC.unLoc x) : addDeclSeparator xs
-      declSeparator (GHC.SigD _ GHC.TypeSig {}) = newline
-      declSeparator (GHC.SigD _ GHC.InlineSig {}) = newline
-      declSeparator (GHC.SigD _ GHC.PatSynSig {}) = newline
-      declSeparator _ = blankline
-      declsExist = not . null . GHC.hsmodDecls
-      prettyImports = importDecls >>= blanklined . fmap outputImportGroup
-      outputImportGroup = lined . fmap pretty
-      importDecls =
-        gets (configSortImports . psConfig) >>= \case
-          True -> pure $ extractImportsSorted m
-          False -> pure $ extractImports m
-#endif
+
 mkModule :: GHC.HsModule' -> WithComments Module
 mkModule m = fromEpAnn (GHC.getModuleAnn m) $ Module {..}
   where
