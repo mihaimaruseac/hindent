1c14459d4de9e3c01f5a91fcc396b8c5a18d10f2
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-27 09:14:58 +0900

    Format

--- src/HIndent/Ast/Module.hs
+++ src/HIndent/Ast/Module.hs
@@ -5,11 +5,11 @@ module HIndent.Ast.Module
   , mkModule
   ) where
 
-import           HIndent.Ast.WithComments
+import HIndent.Ast.WithComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
-import           HIndent.Pretty
-import           HIndent.Pretty.NodeComments
-import           HIndent.Pretty.Types
+import HIndent.Pretty
+import HIndent.Pretty.NodeComments
+import HIndent.Pretty.Types
 
 newtype Module =
   Module GHC.HsModule'
--- src/HIndent/Ast/WithComments.hs
+++ src/HIndent/Ast/WithComments.hs
@@ -1,22 +1,24 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module HIndent.Ast.WithComments
   ( WithComments
   , mkWithComments
   ) where
 
-import           HIndent.Pretty
-import           HIndent.Pretty.NodeComments
-import           HIndent.Pretty.Types
+import HIndent.Pretty
+import HIndent.Pretty.NodeComments
+import HIndent.Pretty.Types
 
 data WithComments a = WithComments
   { comments :: NodeComments
-  , _node    :: a
+  , node :: a
   }
 
 instance CommentExtraction (WithComments a) where
   nodeComments = comments
 
 instance (Pretty a) => Pretty (WithComments a) where
-  pretty' (WithComments _ x) = pretty' x
+  pretty' WithComments {..} = pretty' node
 
 mkWithComments :: a -> WithComments a
 mkWithComments = WithComments (NodeComments [] [] [])
