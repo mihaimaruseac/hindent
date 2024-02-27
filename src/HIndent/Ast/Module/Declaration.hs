64b0be992ba12eebccaa330f62983d1affe820fe
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-27 11:43:40 +0900

    `CommentExtraction`

--- src/HIndent/Ast/Module/Declaration.hs
+++ src/HIndent/Ast/Module/Declaration.hs
@@ -6,13 +6,18 @@ module HIndent.Ast.Module.Declaration
   ) where
 
 import HIndent.Ast.Module.Name
+import HIndent.Ast.NodeComments
 import HIndent.Ast.WithComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
+import HIndent.Pretty.NodeComments
 
 data ModuleDeclaration = ModuleDeclaration
   { name :: WithComments ModuleName
   , exports :: Maybe (GHC.LocatedL [GHC.LIE GHC.GhcPs])
   }
+
+instance CommentExtraction ModuleDeclaration where
+  nodeComments ModuleDeclaration {} = NodeComments [] [] []
 
 mkModuleDeclaration :: GHC.HsModule' -> Maybe ModuleDeclaration
 mkModuleDeclaration GHC.HsModule {..} =
