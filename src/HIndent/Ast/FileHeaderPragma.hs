811ce94dae91609fc0a18260a5be1b2039fd224e
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-29 18:55:09 +0900

    Get pragmas from a `EpaCommentTok`, not a `String`

--- src/HIndent/Ast/FileHeaderPragma.hs
+++ src/HIndent/Ast/FileHeaderPragma.hs
@@ -3,10 +3,16 @@ module HIndent.Ast.FileHeaderPragma
   , mkFileHeaderPragma
   ) where
 
-import HIndent.Ast.NodeComments
-import HIndent.Pretty
-import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
+import           Data.Bifunctor
+import           Data.Char
+import           Data.List
+import           Data.List.Split
+import qualified GHC.Hs                      as GHC
+import           HIndent.Ast.NodeComments
+import           HIndent.Pragma
+import           HIndent.Pretty
+import           HIndent.Pretty.Combinators
+import           HIndent.Pretty.NodeComments
 
 newtype FileHeaderPragma =
   FileHeaderPragma String
@@ -17,5 +23,21 @@ instance CommentExtraction FileHeaderPragma where
 instance Pretty FileHeaderPragma where
   pretty' (FileHeaderPragma x) = string x
 
-mkFileHeaderPragma :: String -> FileHeaderPragma
-mkFileHeaderPragma = FileHeaderPragma
+mkFileHeaderPragma :: GHC.EpaCommentTok -> Maybe FileHeaderPragma
+mkFileHeaderPragma =
+  fmap (FileHeaderPragma . uncurry constructPragma) . extractPragma
+
+-- | This function returns a 'Just' value with the pragma
+-- extracted from the passed 'EpaCommentTok' if it has one. Otherwise, it
+-- returns a 'Nothing'.
+extractPragma :: GHC.EpaCommentTok -> Maybe (String, [String])
+extractPragma (GHC.EpaBlockComment c) =
+  second (fmap strip . splitOn ",") <$> extractPragmaNameAndElement c
+  where
+    strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+extractPragma _ = Nothing
+
+-- | Construct a pragma.
+constructPragma :: String -> [String] -> String
+constructPragma optionOrPragma xs =
+  "{-# " ++ fmap toUpper optionOrPragma ++ " " ++ intercalate ", " xs ++ " #-}"
--- src/HIndent/Ast/FileHeaderPragma/Collection.hs
+++ src/HIndent/Ast/FileHeaderPragma/Collection.hs
@@ -6,19 +6,14 @@ module HIndent.Ast.FileHeaderPragma.Collection
   , hasPragmas
   ) where
 
-import Data.Bifunctor
-import Data.Char
-import Data.List
-import Data.List.Split
-import Data.Maybe
-import Generics.SYB
-import HIndent.Ast.FileHeaderPragma
-import HIndent.Ast.NodeComments
+import           Data.Maybe
+import           Generics.SYB
+import           HIndent.Ast.FileHeaderPragma
+import           HIndent.Ast.NodeComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
-import HIndent.Pragma
-import HIndent.Pretty
-import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
+import           HIndent.Pretty
+import           HIndent.Pretty.Combinators
+import           HIndent.Pretty.NodeComments
 
 newtype FileHeaderPragmaCollection =
   FileHeaderPragmaCollection [FileHeaderPragma]
@@ -31,39 +26,16 @@ instance Pretty FileHeaderPragmaCollection where
 
 mkFileHeaderPragmaCollection :: GHC.HsModule' -> FileHeaderPragmaCollection
 mkFileHeaderPragmaCollection =
-  FileHeaderPragmaCollection . fmap mkFileHeaderPragma . collectPragmas
+  FileHeaderPragmaCollection .
+  mapMaybe mkFileHeaderPragma . collectBlockComments
 
 hasPragmas :: FileHeaderPragmaCollection -> Bool
 hasPragmas (FileHeaderPragmaCollection xs) = not $ null xs
 
--- | This function collects pragma comments from the
--- given module and modifies them into 'String's.
---
--- A pragma's name is converted to the @SHOUT_CASE@ (e.g., @lAnGuAgE@ ->
--- @LANGUAGE@).
-collectPragmas :: GHC.HsModule' -> [String]
-collectPragmas =
-  fmap (uncurry constructPragma)
-    . mapMaybe extractPragma
-    . listify isBlockComment
-    . GHC.getModuleAnn
-
--- | This function returns a 'Just' value with the pragma
--- extracted from the passed 'EpaCommentTok' if it has one. Otherwise, it
--- returns a 'Nothing'.
-extractPragma :: GHC.EpaCommentTok -> Maybe (String, [String])
-extractPragma (GHC.EpaBlockComment c) =
-  second (fmap strip . splitOn ",") <$> extractPragmaNameAndElement c
-  where
-    strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
-extractPragma _ = Nothing
-
--- | Construct a pragma.
-constructPragma :: String -> [String] -> String
-constructPragma optionOrPragma xs =
-  "{-# " ++ fmap toUpper optionOrPragma ++ " " ++ intercalate ", " xs ++ " #-}"
+collectBlockComments :: GHC.HsModule' -> [GHC.EpaCommentTok]
+collectBlockComments = listify isBlockComment . GHC.getModuleAnn
 
 -- | Checks if the given comment is a block one.
 isBlockComment :: GHC.EpaCommentTok -> Bool
 isBlockComment GHC.EpaBlockComment {} = True
-isBlockComment _ = False
+isBlockComment _                      = False
