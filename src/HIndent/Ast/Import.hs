6efdeebc48ce4974021508a88772acbe4598e1d5
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-05 11:49:13 +0900

    Split

--- src/HIndent/Ast/Import.hs
+++ src/HIndent/Ast/Import.hs
@@ -8,11 +8,8 @@ module HIndent.Ast.Import
   ) where
 
 import           Control.Monad
-import           Data.Char
 import           Data.Function
 import           Data.List
-import           Data.Maybe
-import qualified GHC.Types.SrcLoc                    as GHC
 import qualified GHC.Unit                            as GHC
 import           HIndent.Applicative
 import           HIndent.Ast.Import.Entry.Collection
@@ -66,17 +63,6 @@ mkImport decl@GHC.ImportDecl {..} = Import {..}
 sortByName :: [WithComments Import] -> [WithComments Import]
 sortByName = fmap sortExplicitImportsInDecl . sortByModuleName
 
--- | The letter type of a 'Char'.
---
--- The order of constructors is important. HIndent sorts explicit imports
--- from ones starting from a capital letter (e.g., data constructors),
--- symbol identifiers, and functions.
-data LetterType
-  = Capital
-  | Symbol
-  | Lower
-  deriving (Eq, Ord)
-
 -- | This function sorts import declarations by their module names.
 sortByModuleName :: [WithComments Import] -> [WithComments Import]
 sortByModuleName = sortBy (compare `on` moduleName . getNode)
@@ -92,79 +78,9 @@ sortExplicitImportsInDecl (L l d@ImportDecl {ideclImportList = Just (x, imports)
 #else
 sortExplicitImportsInDecl = fmap f
   where
-    f (Import {importEntries = Just ImportEntryCollection {..}, ..}) =
-      Import
-        {importEntries = Just ImportEntryCollection {entries = sorted, ..}, ..}
+    f (Import {importEntries = Just xs, ..}) =
+      Import {importEntries = Just sorted, ..}
       where
-        sorted = fmap (fmap sortVariants . sortExplicitImports) entries
+        sorted = sortEntriesByName xs
     f x = x
 #endif
--- | This function sorts the given explicit imports by their names.
-sortExplicitImports :: [GHC.LIE GHC.GhcPs] -> [GHC.LIE GHC.GhcPs]
-sortExplicitImports = sortBy compareImportEntities
-
--- | This function sorts variants (e.g., data constructors and class
--- methods) in the given explicit import by their names.
-sortVariants :: GHC.LIE GHC.GhcPs -> GHC.LIE GHC.GhcPs
-sortVariants (GHC.L l (GHC.IEThingWith x x' x'' xs)) =
-  GHC.L l $ GHC.IEThingWith x x' x'' (sortWrappedNames xs)
-  where
-    sortWrappedNames = sortBy (compare `on` showOutputable)
-sortVariants x = x
-
--- | This function compares two import declarations by their module names.
-compareImportEntities :: GHC.LIE GHC.GhcPs -> GHC.LIE GHC.GhcPs -> Ordering
-compareImportEntities (GHC.L _ a) (GHC.L _ b) =
-  fromMaybe LT $ compareIdentifier <$> getModuleName a <*> getModuleName b
-
--- | This function returns a 'Just' value with the module name extracted
--- from the import declaration. Otherwise, it returns a 'Nothing'.
-getModuleName :: GHC.IE GHC.GhcPs -> Maybe String
-getModuleName (GHC.IEVar _ wrapped)           = Just $ showOutputable wrapped
-getModuleName (GHC.IEThingAbs _ wrapped)      = Just $ showOutputable wrapped
-getModuleName (GHC.IEThingAll _ wrapped)      = Just $ showOutputable wrapped
-getModuleName (GHC.IEThingWith _ wrapped _ _) = Just $ showOutputable wrapped
-getModuleName _                               = Nothing
-
--- | This function compares two identifiers in order of capitals, symbols,
--- and lowers.
-compareIdentifier :: String -> String -> Ordering
-compareIdentifier as@(a:_) bs@(b:_) =
-  case compareChar a b of
-    EQ -> compareSameIdentifierType as bs
-    x  -> x
-compareIdentifier _ _ = error "Either identifier is an empty string."
-
--- | Almost similar to 'compare' but ignores parentheses for symbol
--- identifiers as they are enclosed by parentheses.
-compareSameIdentifierType :: String -> String -> Ordering
-compareSameIdentifierType "" "" = EQ
-compareSameIdentifierType "" _ = LT
-compareSameIdentifierType _ "" = GT
-compareSameIdentifierType ('(':as) bs = compareSameIdentifierType as bs
-compareSameIdentifierType (')':as) bs = compareSameIdentifierType as bs
-compareSameIdentifierType as ('(':bs) = compareSameIdentifierType as bs
-compareSameIdentifierType as (')':bs) = compareSameIdentifierType as bs
-compareSameIdentifierType (a:as) (b:bs) =
-  case compare a b of
-    EQ -> compareSameIdentifierType as bs
-    x  -> x
-
--- | This function compares two characters by their types (capital, symbol,
--- and lower). If both are the same type, then it compares them by the
--- usual ordering.
-compareChar :: Char -> Char -> Ordering
-compareChar a b =
-  case compare at bt of
-    EQ -> compare a b
-    x  -> x
-  where
-    at = charToLetterType a
-    bt = charToLetterType b
-
--- | This function returns a 'LetterType' based on the given character.
-charToLetterType :: Char -> LetterType
-charToLetterType c
-  | isLower c = Lower
-  | isUpper c = Capital
-  | otherwise = Symbol
--- src/HIndent/Ast/Import/Entry/Collection.hs
+++ src/HIndent/Ast/Import/Entry/Collection.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module HIndent.Ast.Import.Entry.Collection
-  ( ImportEntryCollection(..)
+  ( ImportEntryCollection
   , mkImportEntryCollection
+  , sortEntriesByName
   ) where
 
 import           Control.Monad
+import           Data.Char
+import           Data.Function
+import           Data.List
+import           Data.Maybe
 import qualified GHC.Hs                               as GHC
 import qualified GHC.Types.SrcLoc                     as GHC
 import           HIndent.Ast.Import.ImportingOrHiding
@@ -38,3 +43,89 @@ mkImportEntryCollection GHC.ImportDecl {..} =
       Just $ ImportEntryCollection {entries = xs, kind = Importing}
     Just (True, xs) ->
       Just $ ImportEntryCollection {entries = xs, kind = Hiding}
+
+sortEntriesByName :: ImportEntryCollection -> ImportEntryCollection
+sortEntriesByName ImportEntryCollection {..} =
+  ImportEntryCollection
+    {entries = fmap (fmap sortVariants . sortExplicitImports) entries, ..}
+
+-- | This function sorts the given explicit imports by their names.
+sortExplicitImports :: [GHC.LIE GHC.GhcPs] -> [GHC.LIE GHC.GhcPs]
+sortExplicitImports = sortBy compareImportEntities
+
+-- | This function sorts variants (e.g., data constructors and class
+-- methods) in the given explicit import by their names.
+sortVariants :: GHC.LIE GHC.GhcPs -> GHC.LIE GHC.GhcPs
+sortVariants (GHC.L l (GHC.IEThingWith x x' x'' xs)) =
+  GHC.L l $ GHC.IEThingWith x x' x'' (sortWrappedNames xs)
+  where
+    sortWrappedNames = sortBy (compare `on` showOutputable)
+sortVariants x = x
+
+-- | This function compares two import declarations by their module names.
+compareImportEntities :: GHC.LIE GHC.GhcPs -> GHC.LIE GHC.GhcPs -> Ordering
+compareImportEntities (GHC.L _ a) (GHC.L _ b) =
+  fromMaybe LT $ compareIdentifier <$> getModuleName a <*> getModuleName b
+
+-- | This function returns a 'Just' value with the module name extracted
+-- from the import declaration. Otherwise, it returns a 'Nothing'.
+getModuleName :: GHC.IE GHC.GhcPs -> Maybe String
+getModuleName (GHC.IEVar _ wrapped)           = Just $ showOutputable wrapped
+getModuleName (GHC.IEThingAbs _ wrapped)      = Just $ showOutputable wrapped
+getModuleName (GHC.IEThingAll _ wrapped)      = Just $ showOutputable wrapped
+getModuleName (GHC.IEThingWith _ wrapped _ _) = Just $ showOutputable wrapped
+getModuleName _                               = Nothing
+
+-- | This function compares two identifiers in order of capitals, symbols,
+-- and lowers.
+compareIdentifier :: String -> String -> Ordering
+compareIdentifier as@(a:_) bs@(b:_) =
+  case compareChar a b of
+    EQ -> compareSameIdentifierType as bs
+    x  -> x
+compareIdentifier _ _ = error "Either identifier is an empty string."
+
+-- | Almost similar to 'compare' but ignores parentheses for symbol
+-- identifiers as they are enclosed by parentheses.
+compareSameIdentifierType :: String -> String -> Ordering
+compareSameIdentifierType "" "" = EQ
+compareSameIdentifierType "" _ = LT
+compareSameIdentifierType _ "" = GT
+compareSameIdentifierType ('(':as) bs = compareSameIdentifierType as bs
+compareSameIdentifierType (')':as) bs = compareSameIdentifierType as bs
+compareSameIdentifierType as ('(':bs) = compareSameIdentifierType as bs
+compareSameIdentifierType as (')':bs) = compareSameIdentifierType as bs
+compareSameIdentifierType (a:as) (b:bs) =
+  case compare a b of
+    EQ -> compareSameIdentifierType as bs
+    x  -> x
+
+-- | This function compares two characters by their types (capital, symbol,
+-- and lower). If both are the same type, then it compares them by the
+-- usual ordering.
+compareChar :: Char -> Char -> Ordering
+compareChar a b =
+  case compare at bt of
+    EQ -> compare a b
+    x  -> x
+  where
+    at = charToLetterType a
+    bt = charToLetterType b
+
+-- | This function returns a 'LetterType' based on the given character.
+charToLetterType :: Char -> LetterType
+charToLetterType c
+  | isLower c = Lower
+  | isUpper c = Capital
+  | otherwise = Symbol
+
+-- | The letter type of a 'Char'.
+--
+-- The order of constructors is important. HIndent sorts explicit imports
+-- from ones starting from a capital letter (e.g., data constructors),
+-- symbol identifiers, and functions.
+data LetterType
+  = Capital
+  | Symbol
+  | Lower
+  deriving (Eq, Ord)
