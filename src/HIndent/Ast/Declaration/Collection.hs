3581dfb5ab6463e74196471f36a0dcefe8299ad5
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-06 09:07:34 +0900

    `WithComments`

--- src/HIndent/Ast/Declaration/Collection.hs
+++ src/HIndent/Ast/Declaration/Collection.hs
@@ -6,37 +6,38 @@ module HIndent.Ast.Declaration.Collection
   , hasDeclarations
   ) where
 
-import Data.Maybe
-import qualified GHC.Hs as GHC
-import qualified GHC.Types.SrcLoc as GHC
-import HIndent.Ast.NodeComments
+import           Data.Maybe
+import qualified GHC.Hs                             as GHC
+import           HIndent.Ast.NodeComments
+import           HIndent.Ast.WithComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
-import HIndent.Pretty
-import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
+import           HIndent.Pretty
+import           HIndent.Pretty.Combinators
+import           HIndent.Pretty.NodeComments
 
 newtype DeclarationCollection =
-  DeclarationCollection [GHC.LHsDecl GHC.GhcPs]
+  DeclarationCollection [WithComments (GHC.HsDecl GHC.GhcPs)]
 
 instance CommentExtraction DeclarationCollection where
   nodeComments DeclarationCollection {} = NodeComments [] [] []
 
 instance Pretty DeclarationCollection where
   pretty' (DeclarationCollection decls) =
-    mapM_ (\(x, sp) -> pretty x >> fromMaybe (return ()) sp)
-      $ addDeclSeparator decls
+    mapM_ (\(x, sp) -> pretty x >> fromMaybe (return ()) sp) $
+    addDeclSeparator decls
     where
       addDeclSeparator [] = []
       addDeclSeparator [x] = [(x, Nothing)]
       addDeclSeparator (x:xs) =
-        (x, Just $ declSeparator $ GHC.unLoc x) : addDeclSeparator xs
-      declSeparator (GHC.SigD _ GHC.TypeSig {}) = newline
+        (x, Just $ declSeparator $ getNode x) : addDeclSeparator xs
+      declSeparator (GHC.SigD _ GHC.TypeSig {})   = newline
       declSeparator (GHC.SigD _ GHC.InlineSig {}) = newline
       declSeparator (GHC.SigD _ GHC.PatSynSig {}) = newline
-      declSeparator _ = blankline
+      declSeparator _                             = blankline
 
 mkDeclarationCollection :: GHC.HsModule' -> DeclarationCollection
-mkDeclarationCollection GHC.HsModule {..} = DeclarationCollection hsmodDecls
+mkDeclarationCollection GHC.HsModule {..} =
+  DeclarationCollection $ fromGenLocated <$> hsmodDecls
 
 hasDeclarations :: DeclarationCollection -> Bool
 hasDeclarations (DeclarationCollection xs) = not $ null xs
