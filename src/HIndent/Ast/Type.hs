5214e1177e4b5241d5c7f2f5f4f50660cfdaf638
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-07 09:10:32 +0900

    Destruct `HsType`

--- src/HIndent/Ast/Type.hs
+++ src/HIndent/Ast/Type.hs
@@ -3,10 +3,10 @@ module HIndent.Ast.Type
   , mkType
   ) where
 
-import HIndent.Ast.NodeComments
+import           HIndent.Ast.NodeComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
-import HIndent.Pretty
-import HIndent.Pretty.NodeComments
+import           HIndent.Pretty
+import           HIndent.Pretty.NodeComments
 
 newtype Type =
   Type (GHC.HsType GHC.GhcPs)
@@ -18,4 +18,26 @@ instance Pretty Type where
   pretty' (Type x) = pretty x
 
 mkType :: GHC.HsType GHC.GhcPs -> Type
-mkType = Type
+mkType x@GHC.HsForAllTy {}        = Type x
+mkType x@GHC.HsQualTy {}          = Type x
+mkType x@GHC.HsTyVar {}           = Type x
+mkType x@GHC.HsAppTy {}           = Type x
+mkType x@GHC.HsAppKindTy {}       = Type x
+mkType x@GHC.HsFunTy {}           = Type x
+mkType x@GHC.HsListTy {}          = Type x
+mkType x@GHC.HsTupleTy {}         = Type x
+mkType x@GHC.HsSumTy {}           = Type x
+mkType x@GHC.HsOpTy {}            = Type x
+mkType x@GHC.HsParTy {}           = Type x
+mkType x@GHC.HsIParamTy {}        = Type x
+mkType x@GHC.HsStarTy {}          = Type x
+mkType x@GHC.HsKindSig {}         = Type x
+mkType x@GHC.HsSpliceTy {}        = Type x
+mkType x@GHC.HsDocTy {}           = Type x
+mkType x@GHC.HsBangTy {}          = Type x
+mkType x@GHC.HsRecTy {}           = Type x
+mkType x@GHC.HsExplicitListTy {}  = Type x
+mkType x@GHC.HsExplicitTupleTy {} = Type x
+mkType x@GHC.HsTyLit {}           = Type x
+mkType x@GHC.HsWildCardTy {}      = Type x
+mkType x@GHC.XHsType {}           = Type x
