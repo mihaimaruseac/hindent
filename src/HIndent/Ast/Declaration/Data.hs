096284aed1238bea8d1e53f2eea90fa71fb7d0d6
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-07 10:01:39 +0900

    Inline

--- src/HIndent/Ast/Declaration/Data.hs
+++ src/HIndent/Ast/Declaration/Data.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP             #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module HIndent.Ast.Declaration.Data
@@ -5,10 +6,13 @@ module HIndent.Ast.Declaration.Data
   , mkData
   ) where
 
+import           HIndent.Applicative
 import           HIndent.Ast.NodeComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
 import           HIndent.Pretty
+import           HIndent.Pretty.Combinators
 import           HIndent.Pretty.NodeComments
+import           HIndent.Pretty.Types
 
 data Data = Data
   { decl :: GHC.TyClDecl GHC.GhcPs
@@ -16,9 +20,55 @@ data Data = Data
 
 instance CommentExtraction Data where
   nodeComments (Data _) = NodeComments [] [] []
-
+#if MIN_VERSION_ghc_lib_parser(9,6,1)
 instance Pretty Data where
-  pretty' (Data x) = pretty x
-
+  pretty' Data {decl = DataDecl {..}} = do
+    printDataNewtype |=> do
+      whenJust (dd_ctxt tcdDataDefn) $ \x -> do
+        pretty $ Context x
+        string " =>"
+        newline
+      pretty tcdLName
+    spacePrefixed $ pretty <$> hsq_explicit tcdTyVars
+    pretty tcdDataDefn
+    where
+      printDataNewtype =
+        case dd_cons tcdDataDefn of
+          DataTypeCons {} -> string "data "
+          NewTypeCon {}   -> string "newtype "
+#elif MIN_VERSION_ghc_lib_parser(9,4,1)
+instance Pretty Data where
+  pretty' Data {decl = DataDecl {..}} = do
+    printDataNewtype |=> do
+      whenJust (dd_ctxt tcdDataDefn) $ \x -> do
+        pretty $ Context x
+        string " =>"
+        newline
+      pretty tcdLName
+    spacePrefixed $ pretty <$> hsq_explicit tcdTyVars
+    pretty tcdDataDefn
+    where
+      printDataNewtype =
+        case dd_ND tcdDataDefn of
+          DataType -> string "data "
+          NewType  -> string "newtype "
+#else
+instance Pretty Data where
+  pretty' Data {decl = GHC.DataDecl {..}} = do
+    printDataNewtype |=> do
+      whenJust (GHC.dd_ctxt tcdDataDefn) $ \_ -> do
+        pretty $ Context $ GHC.dd_ctxt tcdDataDefn
+        string " =>"
+        newline
+      pretty tcdLName
+    spacePrefixed $ pretty <$> GHC.hsq_explicit tcdTyVars
+    pretty tcdDataDefn
+    where
+      printDataNewtype =
+        case GHC.dd_ND tcdDataDefn of
+          GHC.DataType -> string "data "
+          GHC.NewType  -> string "newtype "
+  pretty' _ = error "Not a data declaration."
+#endif
 mkData :: GHC.TyClDecl GHC.GhcPs -> Data
 mkData decl = Data {..}
