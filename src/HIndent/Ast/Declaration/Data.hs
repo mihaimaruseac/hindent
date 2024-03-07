c2f9aa4166b9e65a6d48eafc48558dcd4f81d25a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-07 10:24:01 +0900

    Inline

--- src/HIndent/Ast/Declaration/Data.hs
+++ src/HIndent/Ast/Declaration/Data.hs
@@ -6,6 +6,9 @@ module HIndent.Ast.Declaration.Data
   , mkDataDeclaration
   ) where
 
+import           Control.Monad
+import qualified GHC.Types.SrcLoc                       as GHC
+import           HIndent.Applicative
 import           HIndent.Ast.Declaration.Data.NewOrData
 import           HIndent.Ast.NodeComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs     as GHC
@@ -56,14 +59,50 @@ instance Pretty Data where
           NewType  -> string "newtype "
 #else
 instance Pretty DataDeclaration where
-  pretty' DataDeclaration {decl = GHC.DataDecl {..}, ..} = do
+  pretty' DataDeclaration { decl = GHC.DataDecl { tcdDataDefn = GHC.HsDataDefn {..}
+                                                , ..
+                                                }
+                          , ..
+                          } = do
     (pretty newOrData >> space) |=> do
       case context of
         Context (Just _) -> pretty context >> string " =>" >> newline
         Context Nothing  -> pure ()
       pretty tcdLName
     spacePrefixed $ pretty <$> GHC.hsq_explicit tcdTyVars
-    pretty tcdDataDefn
+    if isGADT
+      then do
+        whenJust dd_kindSig $ \x -> do
+          string " :: "
+          pretty x
+        string " where"
+        indentedBlock $ newlinePrefixed $ fmap pretty dd_cons
+      else do
+        case dd_cons of
+          [] -> indentedBlock derivingsAfterNewline
+          [x@(GHC.L _ GHC.ConDeclH98 {con_args = GHC.RecCon {}})] -> do
+            string " = "
+            pretty x
+            unless (null dd_derivs) $ space |=> printDerivings
+          [x] -> do
+            string " ="
+            newline
+            indentedBlock $ do
+              pretty x
+              derivingsAfterNewline
+          _ ->
+            indentedBlock $ do
+              newline
+              string "= " |=> vBarSep (fmap pretty dd_cons)
+              derivingsAfterNewline
+    where
+      isGADT =
+        case dd_cons of
+          (GHC.L _ GHC.ConDeclGADT {}:_) -> True
+          _                              -> False
+      derivingsAfterNewline =
+        unless (null dd_derivs) $ newline >> printDerivings
+      printDerivings = lined $ fmap pretty dd_derivs
   pretty' _ = error "Not a data declaration."
 #endif
 mkDataDeclaration :: GHC.TyClDecl GHC.GhcPs -> DataDeclaration
