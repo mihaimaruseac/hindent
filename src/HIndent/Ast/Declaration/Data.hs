e21cb03142f2c2ccf408fcfcc823d242fed0b09f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-08 11:54:40 +0900

    Remove CPP blocks just for now

--- src/HIndent/Ast/Declaration/Data.hs
+++ src/HIndent/Ast/Declaration/Data.hs
@@ -44,39 +44,7 @@ data DataDeclaration
 instance CommentExtraction DataDeclaration where
   nodeComments GADT {}   = NodeComments [] [] []
   nodeComments Record {} = NodeComments [] [] []
-#if MIN_VERSION_ghc_lib_parser(9,6,1)
-instance Pretty Data where
-  pretty' Data {decl = DataDecl {..}} = do
-    printDataNewtype |=> do
-      whenJust (dd_ctxt tcdDataDefn) $ \x -> do
-        pretty $ Context x
-        string " =>"
-        newline
-      pretty tcdLName
-    spacePrefixed $ pretty <$> hsq_explicit tcdTyVars
-    pretty tcdDataDefn
-    where
-      printDataNewtype =
-        case dd_cons tcdDataDefn of
-          DataTypeCons {} -> string "data "
-          NewTypeCon {}   -> string "newtype "
-#elif MIN_VERSION_ghc_lib_parser(9,4,1)
-instance Pretty Data where
-  pretty' Data {decl = DataDecl {..}} = do
-    printDataNewtype |=> do
-      whenJust (dd_ctxt tcdDataDefn) $ \x -> do
-        pretty $ Context x
-        string " =>"
-        newline
-      pretty tcdLName
-    spacePrefixed $ pretty <$> hsq_explicit tcdTyVars
-    pretty tcdDataDefn
-    where
-      printDataNewtype =
-        case dd_ND tcdDataDefn of
-          DataType -> string "data "
-          NewType  -> string "newtype "
-#else
+
 instance Pretty DataDeclaration where
   pretty' GADT {..} = do
     pretty header
@@ -108,7 +76,7 @@ instance Pretty DataDeclaration where
         unless (null dd_derivs) $ newline >> printDerivings
       printDerivings = lined $ fmap pretty dd_derivs
   pretty' _ = error "Not a data declaration."
-#endif
+
 mkDataDeclaration :: GHC.TyClDecl GHC.GhcPs -> Maybe DataDeclaration
 mkDataDeclaration decl@GHC.DataDecl {tcdDataDefn = GHC.HsDataDefn {..}}
   | Just header <- mkHeader decl =
@@ -126,51 +94,6 @@ mkDataDeclaration decl@GHC.DataDecl {tcdDataDefn = GHC.HsDataDefn {..}}
 mkDataDeclaration _ = Nothing
 
 prettyGADT :: GADTConstructor -> Printer ()
-#if MIN_VERSION_ghc_lib_parser(9,6,1)
-prettyGADT ConDeclGADT {..} = do
-  hCommaSep $ fmap pretty $ NonEmpty.toList con_names
-  hor <-|> ver
-  where
-    hor = string " :: " |=> body
-    ver = do
-      newline
-      indentedBlock (string ":: " |=> body)
-    body =
-      case (forallNeeded, con_mb_cxt) of
-        (True, Just ctx)  -> withForallCtx ctx
-        (True, Nothing)   -> withForallOnly
-        (False, Just ctx) -> withCtxOnly ctx
-        (False, Nothing)  -> noForallCtx
-    withForallOnly = do
-      pretty con_bndrs
-      (space >> horArgs) <-|> (newline >> verArgs)
-    noForallCtx = horArgs <-|> verArgs
-    withForallCtx ctx = do
-      pretty con_bndrs
-      (space >> pretty (Context ctx)) <-|> (newline >> pretty (Context ctx))
-      newline
-      prefixed "=> " verArgs
-    withCtxOnly ctx =
-      (pretty (Context ctx) >> string " => " >> horArgs) <-|>
-      (pretty (Context ctx) >> prefixed "=> " verArgs)
-    horArgs =
-      case con_g_args of
-        PrefixConGADT xs ->
-          inter (string " -> ") $
-          fmap (\(HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
-        RecConGADT xs _ -> inter (string " -> ") [recArg xs, pretty con_res_ty]
-    verArgs =
-      case con_g_args of
-        PrefixConGADT xs ->
-          prefixedLined "-> " $
-          fmap (\(HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
-        RecConGADT xs _ -> prefixedLined "-> " [recArg xs, pretty con_res_ty]
-    recArg xs = printCommentsAnd xs $ \xs' -> vFields' $ fmap pretty xs'
-    forallNeeded =
-      case unLoc con_bndrs of
-        HsOuterImplicit {} -> False
-        HsOuterExplicit {} -> True
-#else
 prettyGADT (GADTConstructor GHC.ConDeclGADT {..}) = do
   hCommaSep $ fmap pretty con_names
   hor <-|> ver
@@ -189,61 +112,31 @@ prettyGADT (GADTConstructor GHC.ConDeclGADT {..}) = do
       pretty con_bndrs
       (space >> horArgs) <-|> (newline >> verArgs)
     noForallCtx = horArgs <-|> verArgs
-#if MIN_VERSION_ghc_lib_parser(9,4,1)
-    withForallCtx ctx = do
-      pretty con_bndrs
-      (space >> pretty (Context ctx)) <-|> (newline >> pretty (Context ctx))
-      newline
-      prefixed "=> " verArgs
-
-    withCtxOnly ctx =
-      (pretty (Context ctx) >> string " => " >> horArgs) <-|>
-      (pretty (Context ctx) >> prefixed "=> " verArgs)
-
-    horArgs =
-      case con_g_args of
-        PrefixConGADT xs ->
-          inter (string " -> ") $
-          fmap (\(HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
-        RecConGADT xs _ -> inter (string " -> ") [recArg xs, pretty con_res_ty]
-
-    verArgs =
-      case con_g_args of
-        PrefixConGADT xs ->
-          prefixedLined "-> " $
-          fmap (\(HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
-        RecConGADT xs _ -> prefixedLined "-> " [recArg xs, pretty con_res_ty]
-#else
     withForallCtx _ = do
       pretty con_bndrs
       (space >> pretty (Context con_mb_cxt)) <-|>
         (newline >> pretty (Context con_mb_cxt))
       newline
       prefixed "=> " verArgs
-
     withCtxOnly _ =
       (pretty (Context con_mb_cxt) >> string " => " >> horArgs) <-|>
       (pretty (Context con_mb_cxt) >> prefixed "=> " verArgs)
-
     horArgs =
       case con_g_args of
         GHC.PrefixConGADT xs ->
           inter (string " -> ") $
           fmap (\(GHC.HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
-        GHC.RecConGADT xs -> inter (string " -> ") [recArg xs, pretty con_res_ty]
-
+        GHC.RecConGADT xs ->
+          inter (string " -> ") [recArg xs, pretty con_res_ty]
     verArgs =
       case con_g_args of
         GHC.PrefixConGADT xs ->
           prefixedLined "-> " $
           fmap (\(GHC.HsScaled _ x) -> pretty x) xs ++ [pretty con_res_ty]
         GHC.RecConGADT xs -> prefixedLined "-> " [recArg xs, pretty con_res_ty]
-#endif
     recArg xs = printCommentsAnd xs $ \xs' -> vFields' $ fmap pretty xs'
-
     forallNeeded =
       case GHC.unLoc con_bndrs of
         GHC.HsOuterImplicit {} -> False
         GHC.HsOuterExplicit {} -> True
-#endif
 prettyGADT _ = error "Not a GADT constructor."
