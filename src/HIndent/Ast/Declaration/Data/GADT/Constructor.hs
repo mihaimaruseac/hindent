fbfc7ce2b02842c9ebb08c497369162b01bd121f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-09 13:40:22 +0900

    Fix

--- src/HIndent/Ast/Declaration/Data/GADT/Constructor.hs
+++ src/HIndent/Ast/Declaration/Data/GADT/Constructor.hs
@@ -16,7 +16,7 @@ import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
 import HIndent.Pretty
 import HIndent.Pretty.Combinators
 import HIndent.Pretty.NodeComments
-#if MIN_VERSION_ghc_lib_parser(9, 4, 0)
+#if MIN_VERSION_ghc_lib_parser(9, 6, 0)
 import qualified Data.List.NonEmpty as NE
 #endif
 data GADTConstructor = GADTConstructor
@@ -75,7 +75,7 @@ mkGADTConstructor decl@GHC.ConDeclGADT {..} = Just $ GADTConstructor {..}
 mkGADTConstructor _ = Nothing
 
 getNames :: GHC.ConDecl GHC.GhcPs -> Maybe [WithComments String]
-#if MIN_VERSION_ghc_lib_parser(9, 4, 0)
+#if MIN_VERSION_ghc_lib_parser(9, 6, 0)
 getNames GHC.ConDeclGADT {..} =
   Just $ NE.toList $ fmap (fmap showOutputable . fromGenLocated) con_names
 #else
