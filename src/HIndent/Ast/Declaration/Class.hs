873f62d8f4ffd03e31a2dca95e9c9cc5d737146c
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-14 09:32:20 +0900

    Define `ClassDeclaration`

---
+++ src/HIndent/Ast/Declaration/Class.hs
@@ -0,0 +1,13 @@
+module HIndent.Ast.Declaration.Class
+  ( ClassDeclaration(..)
+  ) where
+
+import           HIndent.Ast.NodeComments
+import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
+import           HIndent.Pretty.NodeComments
+
+newtype ClassDeclaration =
+  ClassDeclaration (GHC.TyClDecl GHC.GhcPs)
+
+instance CommentExtraction ClassDeclaration where
+  nodeComments (ClassDeclaration _) = NodeComments [] [] []
