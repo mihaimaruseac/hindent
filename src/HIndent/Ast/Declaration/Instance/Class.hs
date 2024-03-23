f32020fbb5d2e69ecd0290dde41816cf3360c073
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-24 00:50:18 +0900

    Inline fields into `ClassInstance` (#852)

--- src/HIndent/Ast/Declaration/Instance/Class.hs
+++ src/HIndent/Ast/Declaration/Instance/Class.hs
@@ -7,6 +7,7 @@ module HIndent.Ast.Declaration.Instance.Class
 
 import Control.Monad
 import qualified GHC.Data.Bag as GHC
+import qualified GHC.Types.Basic as GHc
 import HIndent.Applicative
 import HIndent.Ast.NodeComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
@@ -16,14 +17,20 @@ import HIndent.Pretty.NodeComments
 import HIndent.Pretty.SigBindFamily
 import HIndent.Pretty.Types
 
-newtype ClassInstance =
-  ClassInstance (GHC.ClsInstDecl GHC.GhcPs)
+data ClassInstance = ClassInstance
+  { cid_overlap_mode :: Maybe (GHC.XRec GHC.GhcPs GHc.OverlapMode)
+  , cid_sigs :: [GHC.LSig GHC.GhcPs]
+  , cid_binds :: GHC.LHsBinds GHC.GhcPs
+  , cid_tyfam_insts :: [GHC.LTyFamInstDecl GHC.GhcPs]
+  , cid_datafam_insts :: [GHC.LDataFamInstDecl GHC.GhcPs]
+  , cid_poly_ty :: GHC.LHsSigType GHC.GhcPs
+  }
 
 instance CommentExtraction ClassInstance where
   nodeComments ClassInstance {} = NodeComments [] [] []
 
 instance Pretty ClassInstance where
-  pretty' (ClassInstance GHC.ClsInstDecl {..}) = do
+  pretty' (ClassInstance {..}) = do
     string "instance " |=> do
       whenJust cid_overlap_mode $ \x -> do
         pretty x
@@ -43,5 +50,6 @@ instance Pretty ClassInstance where
           cid_datafam_insts
 
 mkClassInstance :: GHC.InstDecl GHC.GhcPs -> Maybe ClassInstance
-mkClassInstance GHC.ClsInstD {..} = Just $ ClassInstance cid_inst
+mkClassInstance GHC.ClsInstD {cid_inst = GHC.ClsInstDecl {..}} =
+  Just $ ClassInstance {..}
 mkClassInstance _ = Nothing
