0900ee9cc51a6e2b2a17d887ec70e818b5a422e8
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-10 12:49:47 +0900

    Use `Expression`

--- src/HIndent/Ast/Declaration/Annotation.hs
+++ src/HIndent/Ast/Declaration/Annotation.hs
@@ -9,6 +9,7 @@ module HIndent.Ast.Declaration.Annotation
 import HIndent.Ast.Declaration.Annotation.Provenance
 import {-# SOURCE #-} HIndent.Ast.Expression
 import HIndent.Ast.NodeComments
+import HIndent.Ast.WithComments
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
 import {-# SOURCE #-} HIndent.Pretty
 import HIndent.Pretty.Combinators
@@ -16,7 +17,7 @@ import HIndent.Pretty.NodeComments
 
 data Annotation = Annotation
   { provenance :: Provenance
-  , expr :: GHC.LHsExpr GHC.GhcPs
+  , expr :: WithComments Expression
   }
 
 instance CommentExtraction Annotation where
@@ -24,18 +25,17 @@ instance CommentExtraction Annotation where
 
 instance Pretty Annotation where
   pretty' Annotation {..} =
-    spaced
-      [ string "{-# ANN"
-      , pretty provenance
-      , pretty $ fmap mkExpression expr
-      , string "#-}"
-      ]
+    spaced [string "{-# ANN", pretty provenance, pretty expr, string "#-}"]
 
 mkAnnotation :: GHC.AnnDecl GHC.GhcPs -> Annotation
 #if MIN_VERSION_ghc_lib_parser(9, 6, 1)
-mkAnnotation (GHC.HsAnnotation _ prov expr) =
-  Annotation {provenance = mkProvenance prov, ..}
+mkAnnotation (GHC.HsAnnotation _ prov ex) = Annotation {..}
+  where
+    provenance = mkProvenance prov
+    expr = fmap mkExpression $ fromGenLocated ex
 #else
-mkAnnotation (GHC.HsAnnotation _ _ prov expr) =
-  Annotation {provenance = mkProvenance prov, ..}
+mkAnnotation (GHC.HsAnnotation _ _ prov ex) = Annotation {..}
+  where
+    provenance = mkProvenance prov
+    expr = fmap mkExpression $ fromGenLocated ex
 #endif
