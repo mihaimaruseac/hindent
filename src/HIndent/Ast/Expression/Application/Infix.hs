4810b43779514cd9269544df556fdc3b945196d6
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-13 14:37:20 +0900

    Simplify

--- src/HIndent/Ast/Expression/Application/Infix.hs
+++ src/HIndent/Ast/Expression/Application/Infix.hs
@@ -1,28 +1,28 @@
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ViewPatterns    #-}
 
 module HIndent.Ast.Expression.Application.Infix
   ( InfixApplication
   , mkInfixApplication
   ) where
 
-import Control.Monad
-import Data.Maybe
-import qualified GHC.Types.Fixity as GHC
-import qualified GHC.Types.SrcLoc as GHC
-import {-# SOURCE #-} HIndent.Ast.Expression
-import HIndent.Ast.NodeComments
-import HIndent.Fixity
-import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
-import {-# SOURCE #-} HIndent.Pretty
-import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
-import HIndent.Pretty.Types
-import qualified Language.Haskell.GhclibParserEx.GHC.Hs.Expr as GHC
+import                          Control.Monad
+import                          Data.Maybe
+import                qualified GHC.Types.Fixity                            as GHC
+import                qualified GHC.Types.SrcLoc                            as GHC
+import {-# SOURCE #-}           HIndent.Ast.Expression
+import                          HIndent.Ast.NodeComments
+import                          HIndent.Fixity
+import                qualified HIndent.GhcLibParserWrapper.GHC.Hs          as GHC
+import {-# SOURCE #-}           HIndent.Pretty
+import                          HIndent.Pretty.Combinators
+import                          HIndent.Pretty.NodeComments
+import                          HIndent.Pretty.Types
+import                qualified Language.Haskell.GhclibParserEx.GHC.Hs.Expr as GHC
 
 data InfixApplication = InfixApplication
   { lhs :: GHC.LHsExpr GHC.GhcPs
-  , op :: GHC.LHsExpr GHC.GhcPs
+  , op  :: GHC.LHsExpr GHC.GhcPs
   , rhs :: GHC.LHsExpr GHC.GhcPs
   }
 
@@ -49,22 +49,12 @@ instance Pretty InfixApplication where
         | GHC.L _ (GHC.OpApp _ _ o _) <- lhs
         , isSameAssoc o = leftAssoc
         | otherwise = rightAssoc
-      prettyOps [l, o, GHC.L _ (GHC.HsDo _ (GHC.DoExpr m) xs)] = do
+      prettyOps [l, o, r@(GHC.L _ (GHC.HsDo {}))] = do
         spaced
           [ pretty $ fmap mkExpression l
           , pretty $ InfixExpr o
-          , pretty $ QualifiedDo m Do
+          , pretty $ fmap mkExpression r
           ]
-        newline
-        indentedBlock $ printCommentsAnd xs (lined . fmap pretty)
-      prettyOps [l, o, GHC.L _ (GHC.HsDo _ (GHC.MDoExpr m) xs)] = do
-        spaced
-          [ pretty $ fmap mkExpression l
-          , pretty $ InfixExpr o
-          , pretty $ QualifiedDo m Mdo
-          ]
-        newline
-        indentedBlock $ printCommentsAnd xs (lined . fmap pretty)
       prettyOps [l, o, r@(GHC.L _ GHC.HsLam {})] = do
         spaced
           [ pretty $ fmap mkExpression l
