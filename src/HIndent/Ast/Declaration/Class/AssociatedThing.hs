82c6edcac0167885d010cc362b021cab3722c5ff
Hiroki Tokunaga <tokusan441@gmail.com>
2025-11-29 13:39:23 +0900

    Rename ClassAssociatedThing constructors

--- src/HIndent/Ast/Declaration/Class/AssociatedThing.hs
+++ src/HIndent/Ast/Declaration/Class/AssociatedThing.hs
@@ -16,36 +16,35 @@ import {-# SOURCE #-} HIndent.Pretty (Pretty(..), pretty)
 import HIndent.Pretty.NodeComments
 
 data ClassAssociatedThing
-  = ClassAssociatedSignature Signature
-  | ClassAssociatedMethod Bind
-  | ClassAssociatedTypeFamily TypeFamily
-  | ClassAssociatedDataFamily DataFamily
-  | ClassAssociatedTypeDefault AssociatedTypeDefault
+  = Sig Signature
+  | Method Bind
+  | TypeFam TypeFamily
+  | DataFam DataFamily
+  | TypeDefault AssociatedTypeDefault
 
 instance CommentExtraction ClassAssociatedThing where
-  nodeComments ClassAssociatedSignature {} = NodeComments [] [] []
-  nodeComments ClassAssociatedMethod {} = NodeComments [] [] []
-  nodeComments ClassAssociatedTypeFamily {} = NodeComments [] [] []
-  nodeComments ClassAssociatedDataFamily {} = NodeComments [] [] []
-  nodeComments ClassAssociatedTypeDefault {} = NodeComments [] [] []
+  nodeComments Sig {} = NodeComments [] [] []
+  nodeComments Method {} = NodeComments [] [] []
+  nodeComments TypeFam {} = NodeComments [] [] []
+  nodeComments DataFam {} = NodeComments [] [] []
+  nodeComments TypeDefault {} = NodeComments [] [] []
 
 instance Pretty ClassAssociatedThing where
-  pretty' (ClassAssociatedSignature sig) = pretty sig
-  pretty' (ClassAssociatedMethod bind) = pretty bind
-  pretty' (ClassAssociatedTypeFamily fam) = pretty fam
-  pretty' (ClassAssociatedDataFamily fam) = pretty fam
-  pretty' (ClassAssociatedTypeDefault defn) = pretty defn
+  pretty' (Sig sig) = pretty sig
+  pretty' (Method bind) = pretty bind
+  pretty' (TypeFam fam) = pretty fam
+  pretty' (DataFam fam) = pretty fam
+  pretty' (TypeDefault defn) = pretty defn
 
--- | Convert a GHC class element into our AST representation.
 mkClassAssociatedThing :: ClassElement -> ClassAssociatedThing
 mkClassAssociatedThing =
   foldClassElement
-    (ClassAssociatedSignature . mkSignature)
-    (ClassAssociatedMethod . mkBind)
+    (Sig . mkSignature)
+    (Method . mkBind)
     convertFamily
-    (ClassAssociatedTypeDefault . mkAssociatedTypeDefault)
+    (TypeDefault . mkAssociatedTypeDefault)
   where
     convertFamily decl
-      | Just fam <- mkTypeFamily decl = ClassAssociatedTypeFamily fam
-      | Just fam <- mkDataFamily decl = ClassAssociatedDataFamily fam
+      | Just fam <- mkTypeFamily decl = TypeFam fam
+      | Just fam <- mkDataFamily decl = DataFam fam
       | otherwise = error "Unreachable"
