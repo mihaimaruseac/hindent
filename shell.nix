2de860b6f90c5584bec392e1c671bb2d5a5ffa2d
Mihai Maruseac <mihai.maruseac@gmail.com>
2026-04-25 16:44:50 -0700

    Switch to using GHC 9.12 in nix-shell
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,12 +1,11 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import <nixpkgs> {};
 in
 
 pkgs.mkShell {
   packages = with pkgs; [
     cabal-install
-    haskell.compiler.ghc981
+    haskell.compiler.ghc912
     zlib.dev
   ];
 
