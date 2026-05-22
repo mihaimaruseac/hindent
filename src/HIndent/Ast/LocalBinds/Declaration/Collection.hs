26ad49e03bb751e1be7bfddacec5cce77453c35f
Hiroki Tokunaga <tokusan441@gmail.com>
2026-05-22 22:50:21 +0900

    refactor: extract LocalBinds declaration collections

---
+++ src/HIndent/Ast/LocalBinds/Declaration/Collection.hs
@@ -0,0 +1,40 @@
+module HIndent.Ast.LocalBinds.Declaration.Collection
+  ( LocalDeclarationCollection
+  , hasLocalDeclarations
+  , mkLocalDeclarationCollection
+  ) where
+
+import Data.Function
+import Data.List (sortBy)
+import qualified GHC.Types.SrcLoc as GHC
+import HIndent.Ast.LocalBinds.Declaration
+import HIndent.Ast.WithComments (WithComments, fromGenLocated)
+import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
+import {-# SOURCE #-} HIndent.Pretty (Pretty(..), pretty)
+import HIndent.Pretty.Combinators
+import HIndent.Pretty.NodeComments (CommentExtraction(..), emptyNodeComments)
+
+newtype LocalDeclarationCollection =
+  LocalDeclarationCollection [WithComments LocalDeclaration]
+
+instance CommentExtraction LocalDeclarationCollection where
+  nodeComments _ = emptyNodeComments
+
+instance Pretty LocalDeclarationCollection where
+  pretty' (LocalDeclarationCollection declarations) =
+    lined $ fmap pretty declarations
+
+mkLocalDeclarationCollection ::
+     [GHC.LSig GHC.GhcPs]
+  -> [GHC.LHsBindLR GHC.GhcPs GHC.GhcPs]
+  -> LocalDeclarationCollection
+mkLocalDeclarationCollection sigs binds =
+  LocalDeclarationCollection
+    $ fmap fromGenLocated
+    $ sortBy (compare `on` GHC.realSrcSpan . GHC.locA . GHC.getLoc)
+    $ fmap (fmap mkLocalSignatureDeclaration) sigs
+        ++ fmap (fmap mkLocalBindingDeclaration) binds
+
+hasLocalDeclarations :: LocalDeclarationCollection -> Bool
+hasLocalDeclarations (LocalDeclarationCollection declarations) =
+  not $ null declarations
--- src/HIndent/Ast/LocalBinds/ImplicitBindings.hs
+++ src/HIndent/Ast/LocalBinds/ImplicitBindings.hs
@@ -7,19 +7,18 @@ import HIndent.Ast.LocalBinds.ImplicitBinding
   ( ImplicitBinding
   , mkImplicitBinding
   )
-import HIndent.Ast.NodeComments (NodeComments(..))
 import HIndent.Ast.WithComments (WithComments, fromGenLocated)
 import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
 import {-# SOURCE #-} HIndent.Pretty (Pretty(..), pretty)
 import HIndent.Pretty.Combinators
-import HIndent.Pretty.NodeComments
+import HIndent.Pretty.NodeComments (CommentExtraction(..), emptyNodeComments)
 
 newtype ImplicitBindings = ImplicitBindings
   { bindings :: [WithComments ImplicitBinding]
   }
 
 instance CommentExtraction ImplicitBindings where
-  nodeComments _ = NodeComments [] [] []
+  nodeComments _ = emptyNodeComments
 
 instance Pretty ImplicitBindings where
   pretty' (ImplicitBindings xs) = lined $ fmap pretty xs
