fad42e291e8db38e94b8c73205de8587e596e443
Hiroki Tokunaga <tokusan441@gmail.com>
2026-05-23 11:15:11 +0900

    refactor: extract class instance body and associated members

---
+++ src/HIndent/Ast/Declaration/Instance/Class/Member.hs
@@ -0,0 +1,51 @@
+module HIndent.Ast.Declaration.Instance.Class.Member
+  ( ClassInstanceMember
+  , mkAssociatedDataInstanceMember
+  , mkAssociatedTypeInstanceMember
+  , mkClassInstanceMethodMember
+  , mkClassInstanceSignatureMember
+  ) where
+
+import HIndent.Ast.Declaration.Bind
+import HIndent.Ast.Declaration.Instance.Family.Data.Associated
+import HIndent.Ast.Declaration.Instance.Family.Type.Associated
+import HIndent.Ast.Declaration.Signature
+import HIndent.Ast.NodeComments
+import qualified HIndent.GhcLibParserWrapper.GHC.Hs as GHC
+import {-# SOURCE #-} HIndent.Pretty
+import HIndent.Pretty.NodeComments
+
+data ClassInstanceMember
+  = AssociatedDataInstance AssociatedDataFamilyInstance
+  | AssociatedTypeInstance AssociatedType
+  | Method Bind
+  | Signature Signature
+
+instance CommentExtraction ClassInstanceMember where
+  nodeComments AssociatedDataInstance {} = NodeComments [] [] []
+  nodeComments AssociatedTypeInstance {} = NodeComments [] [] []
+  nodeComments Method {} = NodeComments [] [] []
+  nodeComments Signature {} = NodeComments [] [] []
+
+instance Pretty ClassInstanceMember where
+  pretty' (AssociatedDataInstance associatedDataInstance) =
+    pretty associatedDataInstance
+  pretty' (AssociatedTypeInstance associatedTypeInstance) =
+    pretty associatedTypeInstance
+  pretty' (Method bind) = pretty bind
+  pretty' (Signature signature) = pretty signature
+
+mkAssociatedDataInstanceMember ::
+     GHC.DataFamInstDecl GHC.GhcPs -> ClassInstanceMember
+mkAssociatedDataInstanceMember =
+  AssociatedDataInstance . mkAssociatedDataFamilyInstance
+
+mkAssociatedTypeInstanceMember ::
+     GHC.TyFamInstDecl GHC.GhcPs -> ClassInstanceMember
+mkAssociatedTypeInstanceMember = AssociatedTypeInstance . mkAssociatedType
+
+mkClassInstanceMethodMember :: GHC.HsBind GHC.GhcPs -> ClassInstanceMember
+mkClassInstanceMethodMember = Method . mkBind
+
+mkClassInstanceSignatureMember :: GHC.Sig GHC.GhcPs -> ClassInstanceMember
+mkClassInstanceSignatureMember = Signature . mkSignature
